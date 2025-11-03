ffb91652d2f5ee8f7f9ce94483f5c2a28ecb7c0e
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-11-03 18:30:08 +0500

    Fix all test cases for Task2

--- Task2/NumberParser.cs
+++ Task2/NumberParser.cs
@@ -6,7 +6,47 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+
+            ArgumentNullException.ThrowIfNull(stringValue, "stringValue");
+
+
+            if (string.IsNullOrWhiteSpace(stringValue))
+            {
+                throw new FormatException("Input string was not in a correct format.");
+            }
+
+            stringValue = stringValue.Trim();
+
+            double num = 0;
+            int sign = 1;
+            int i = 0;
+
+            if (stringValue[0] == '-' || stringValue[0] == '+')
+            {
+                sign = stringValue[0] == '-' ? -1 : 1;
+                i++;
+            }
+
+            while (i < stringValue.Length)
+            {
+                int currentNumber = stringValue[i] - '0';
+
+                if (currentNumber > 9 || currentNumber < 0)
+                {
+                    throw new FormatException("Input string was not in a correct format.");
+                }
+
+                num = num * 10 + currentNumber;
+                i++;
+            }
+
+            checked
+            {
+
+                return (int)(num * sign);
+
+            }
+
         }
     }
 }
